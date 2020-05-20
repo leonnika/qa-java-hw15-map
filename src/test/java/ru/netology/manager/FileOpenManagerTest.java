2771a643f983be68f8028d8e20f4a847ec031fd6
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-21 01:20:38 +0500

    added tests

--- src/test/java/ru/netology/manager/FileOpenManagerTest.java
+++ src/test/java/ru/netology/manager/FileOpenManagerTest.java
@@ -1,6 +1,7 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,6 +12,38 @@ class FileOpenManagerTest {
     private Map<String, String> extensionApp = new HashMap<String, String>();
     private FileOpenManager manager = new FileOpenManager();
 
+    @Nested
+    public class Empty {
+        @Test
+        void shouldAddApp() {
+            manager.addApp("png", "Paint", extensionApp);
+            System.out.println("Test for AddApp, expected 1 elements, actual:" + extensionApp);
+        }
+
+        @Test
+        void shouldGetAppByExtension() {
+            System.out.println("Test for getAppByExtension, expected: null, actual: " + manager.getAppByExtension("doc", extensionApp));
+        }
+
+        @Test
+        void shouldDeleteApp() {
+            manager.deleteApp("png", extensionApp);
+            System.out.println("Test for AddApp, expected null, actual:" + extensionApp);
+        }
+
+        @Test
+        void shouldGetExtensionWithApp() {
+            System.out.println("Test for GetExtensionWithApp, expected null, actual:" + manager.getExtensionWithApp(extensionApp));
+        }
+
+        @Test
+        void shouldGetAppWithExtension() {
+            System.out.println("Test for GetAppWithExtension, expected null, actual:" + manager.getAppWithExtension(extensionApp));
+        }
+    }
+
+    @Nested
+    public class MultipleItem {
     @BeforeEach
     void init() {
         extensionApp.put("doc", "word");
@@ -18,7 +51,6 @@ class FileOpenManagerTest {
         extensionApp.put("txt", "word");
         extensionApp.put("html", "chrome");
     }
-
 
     @Test
     void shouldAddApp() {
@@ -46,4 +78,4 @@ class FileOpenManagerTest {
     void shouldGetAppWithExtension() {
         System.out.println("Test for GetAppWithExtension, expected 3 elements, actual:" + manager.getAppWithExtension(extensionApp));
     }
-}
+}}
