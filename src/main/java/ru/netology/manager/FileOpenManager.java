bac43d374511df4eae8ae44ae98ec43d9e9effa6
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-24 01:31:33 +0500

    changed project

--- src/main/java/ru/netology/manager/FileOpenManager.java
+++ src/main/java/ru/netology/manager/FileOpenManager.java
@@ -1,43 +1,46 @@
 package ru.netology.manager;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class FileOpenManager {
-    //private  Map<String, String> extensionApp = new HashMap<String, String>();
+    private Map<String, String> extensionApp = new HashMap<String, String>();
 
-    public FileOpenManager() {
+    public FileOpenManager(Map<String, String> extensionApp) {
+        this.extensionApp = extensionApp;
     }
 
     //Регистрировать новое приложение для открытия файлов с определённым расширением
-    public void addApp(String extension, String app, Map<String, String> map) {
+    public void addApp(String extension, String app) {
 
-        map.put(extension.toLowerCase(), app.toLowerCase());
+        extensionApp.put(extension.toLowerCase(), app.toLowerCase());
     }
 
     //Получать название приложения, предназначенного для открытия файла с определённым расширением.
-    public String getAppByExtension(String extension, Map<String, String> map) {
-        return map.get(extension.toLowerCase());
+    public String getAppByExtension(String extension) {
+        return extensionApp.get(extension.toLowerCase());
     }
 
     //Удалять привязку приложения к определённому расширению
-    public void deleteApp(String extension, Map<String, String> map) {
-        map.remove(extension.toLowerCase());
+    public void deleteApp(String extension) {
+        extensionApp.remove(extension.toLowerCase());
     }
 
     //Получать список всех зарегистрированных расширений, к которым привязаны приложения для открытия
-    public List<String> getExtensionWithApp(Map<String, String> map) {
-        List<String> result = new ArrayList<String>();
-        for (String k : map.keySet()) {
+    public Set<String> getExtensionWithApp() {
+        Set<String> result = new TreeSet<String>();
+        for (String k : extensionApp.keySet()) {
             result.add(k);
         }
-        Collections.sort(result);
         return result;
     }
 
     //Получать список всех приложений, которые привязаны к каким-либо расширениям
-    public Set<String> getAppWithExtension(Map<String, String> map) {
-         Set<String> result = new TreeSet<String>();
-        for (String k : map.values()) {
+    public Set<String> getAppWithExtension() {
+        Set<String> result = new TreeSet<String>();
+        for (String k : extensionApp.values()) {
             result.add(k);
         }
         return result;
--- src/test/java/ru/netology/manager/FileOpenManagerTest.java
+++ src/test/java/ru/netology/manager/FileOpenManagerTest.java
@@ -6,76 +6,106 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class FileOpenManagerTest {
-
     private Map<String, String> extensionApp = new HashMap<String, String>();
-    private FileOpenManager manager = new FileOpenManager();
+    private FileOpenManager manager = new FileOpenManager(extensionApp);
 
     @Nested
     public class Empty {
         @Test
         void shouldAddApp() {
-            manager.addApp("png", "Paint", extensionApp);
-            System.out.println("Test for AddApp, expected 1 elements, actual:" + extensionApp);
+            Map<String, String> expected = new HashMap<String, String>();
+            expected.put("png", "paint");
+            manager.addApp("png", "paint");
+            assertEquals(expected, extensionApp);
         }
 
         @Test
         void shouldGetAppByExtension() {
-            System.out.println("Test for getAppByExtension, expected: null, actual: " + manager.getAppByExtension("doc", extensionApp));
+            assertNull(manager.getAppByExtension("doc"));
         }
 
         @Test
         void shouldDeleteApp() {
-            manager.deleteApp("png", extensionApp);
-            System.out.println("Test for AddApp, expected null, actual:" + extensionApp);
+            Map<String, String> expected = new HashMap<String, String>();
+            assertEquals(expected, extensionApp);
         }
 
         @Test
         void shouldGetExtensionWithApp() {
-            System.out.println("Test for GetExtensionWithApp, expected null, actual:" + manager.getExtensionWithApp(extensionApp));
+            Set<String> expected = new TreeSet<String>();
+            assertEquals(expected, manager.getExtensionWithApp());
         }
 
         @Test
         void shouldGetAppWithExtension() {
-            System.out.println("Test for GetAppWithExtension, expected null, actual:" + manager.getAppWithExtension(extensionApp));
+            Set<String> expected = new TreeSet<String>();
+            assertEquals(expected, manager.getAppWithExtension());
         }
     }
 
     @Nested
     public class MultipleItem {
-    @BeforeEach
-    void init() {
-        extensionApp.put("doc", "word");
-        extensionApp.put("xls", "excel");
-        extensionApp.put("txt", "word");
-        extensionApp.put("html", "chrome");
-    }
+        @BeforeEach
+        void init() {
+            extensionApp.put("doc", "word");
+            extensionApp.put("xls", "excel");
+            extensionApp.put("txt", "word");
+            extensionApp.put("html", "chrome");
+        }
 
-    @Test
-    void shouldAddApp() {
-        manager.addApp("png", "Paint", extensionApp);
-        System.out.println("Test for AddApp, expected 5 elements, actual:" + extensionApp);
-    }
+        @Test
+        void shouldAddApp() {
+            Map<String, String> expected = new HashMap<String, String>();
+            expected.put("doc", "word");
+            expected.put("xls", "excel");
+            expected.put("txt", "word");
+            expected.put("html", "chrome");
+            expected.put("png", "paint");
+            manager.addApp("png", "Paint");
+            assertEquals(expected, extensionApp);
+        }
 
-    @Test
-    void shouldGetAppByExtension() {
-        System.out.println("Test for getAppByExtension, expected: word, actual: " + manager.getAppByExtension("doc", extensionApp));
-    }
+        @Test
+        void shouldGetAppByExtension() {
+            String expected = "word";
+            assertEquals(expected, manager.getAppByExtension("doc"));
+        }
 
-    @Test
-    void shouldDeleteApp() {
-        manager.deleteApp("png", extensionApp);
-        System.out.println("Test for AddApp, expected 4 elements, actual:" + extensionApp);
-    }
+        @Test
+        void shouldDeleteApp() {
+            Map<String, String> expected = new HashMap<String, String>();
+            expected.put("doc", "word");
+            expected.put("xls", "excel");
+            expected.put("txt", "word");
+            expected.put("html", "chrome");
+            manager.deleteApp("png");
+            assertEquals(expected, extensionApp);
+        }
 
-    @Test
-    void shouldGetExtensionWithApp() {
-        System.out.println("Test for GetExtensionWithApp, expected 4 elements, actual:" + manager.getExtensionWithApp(extensionApp));
-    }
+        @Test
+        void shouldGetExtensionWithApp() {
+            Set<String> expected = new TreeSet<String>();
+            expected.add("doc");
+            expected.add("xls");
+            expected.add("txt");
+            expected.add("html");
+            assertEquals(expected, manager.getExtensionWithApp());
+        }
 
-    @Test
-    void shouldGetAppWithExtension() {
-        System.out.println("Test for GetAppWithExtension, expected 3 elements, actual:" + manager.getAppWithExtension(extensionApp));
+        @Test
+        void shouldGetAppWithExtension() {
+            Set<String> expected = new TreeSet<String>();
+            expected.add("word");
+            expected.add("excel");
+            expected.add("chrome");
+            assertEquals(expected, manager.getAppWithExtension());
+        }
     }
-}}
+}
